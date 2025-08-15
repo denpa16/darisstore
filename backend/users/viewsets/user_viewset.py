af20d88da6555519f4960fe8b7e2f1a09f8d3248
denpa16 <den.pavlov.1998@list.ru>
2025-08-15 17:17:31 +0300

    feat: init

--- backend/users/viewsets/user_viewset.py
+++ backend/users/viewsets/user_viewset.py
@@ -3,75 +3,107 @@ from drf_spectacular.utils import extend_schema
 from rest_framework import status
 from rest_framework.decorators import action
 from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
 from rest_framework.viewsets import GenericViewSet
 
 from common.auth import custom_login
-from common.responses import Response
-from users.serializers import (
-    UserPhoneLoginSerializer,
-    UserPhoneRegisterSerializer,
+
+from ..serializers import (
+    ChangeUserSerializer,
+    UserEmailLoginSerializer,
+    UserLoginOrRegisterSerializer,
     UserSerializer,
 )
-from users.statuses import (
-    USER_NOT_FOUND,
-)
-from users.utils import custom_phone_register
 
 User = get_user_model()
 
 
 @extend_schema(tags=["User"])
 class UserViewSet(GenericViewSet):
-    """API для взаимодействия с личным кабинетом."""
+    """API для взаимодействия с личным кабинетом"""
 
     permission_classes = [AllowAny]
 
     def get_serializer_class(self):
-        """Сериализатор."""
-        if self.action == self.phone_register.__name__:
-            return UserPhoneRegisterSerializer
-        if self.action == self.phone_login.__name__:
-            return UserPhoneLoginSerializer
+        if self.action in ("register_phone", "phone_login"):
+            return UserLoginOrRegisterSerializer
+        if self.action == "change":
+            return ChangeUserSerializer
+        if self.action == "email_login":
+            return UserEmailLoginSerializer
         return UserSerializer
 
     def list(self, request, *args, **kwargs):
-        """Список пользователей."""
         if not bool(request.user and request.user.is_authenticated):
-            return Response(status=status.HTTP_401_UNAUTHORIZED)
+            return Response({"ok": False, "errors": "not authorized"})
         serializer: UserSerializer = self.get_serializer(instance=request.user)
-        return Response(data=serializer.data)
+        return Response(serializer.data)
 
     def retrieve(self, request, *args, **kwargs):
-        """Получение пользователя."""
         if not bool(request.user and request.user.is_authenticated):
-            return Response(status=status.HTTP_401_UNAUTHORIZED)
-        serializer: UserSerializer = self.get_serializer(instance=request.user)
-        return Response(data=serializer.data)
+            return Response({"ok": False, "errors": "not authorized"})
 
-    @action(methods=["POST"], detail=False, url_name="phone_register", permission_classes=[])
-    def phone_register(self, request):
-        """Регистрация пользователя по телефону после валидации кода."""
-        serializer = self.get_serializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        user = custom_phone_register(**serializer.validated_data)
-        if not user:
-            return Response(status=status.HTTP_400_BAD_REQUEST)
+        serializer: UserSerializer = self.get_serializer(instance=request.user)
+        return Response(serializer.data)
+
+    @action(methods=["POST"], detail=False, url_name="register_phone", permission_classes=[])
+    def register_phone(self, request):
+        """Регистрация пользователя после валидации кода"""
+        serializer: UserLoginOrRegisterSerializer = self.get_serializer(data=request.data)
+        if not serializer.is_valid():
+            return Response({"ok": False, "errors": serializer.errors})
+        if User.objects.filter(phone=serializer.validated_data["phone"]).exists():
+            return Response(
+                {
+                    "ok": False,
+                    "errors": "Пользователь с таким номером телефона "
+                    "уже зарегистрирован в системе.",
+                },
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+        user = authenticate(request, **serializer.data)
         custom_login(request, user, backend="users.backends.phone.PhoneBackend")
-        return Response(data=UserSerializer(instance=user).data, status=status.HTTP_201_CREATED)
+        return Response(UserSerializer(instance=user).data)
+
+    @action(methods=["POST"], detail=False, url_name="email_login", permission_classes=[])
+    def email_login(self, request):
+        """Авторизация по почте"""
+        serializer: UserEmailLoginSerializer = self.get_serializer(data=request.data)
+        if not serializer.is_valid():
+            return Response({"ok": False, "errors": serializer.errors})
+        user = authenticate(request, **serializer.data)
+        custom_login(request, user, backend="users.backends.email_backend.EmailBackend")
+        if not user.is_anonymous and user.is_authenticated:
+            return Response(
+                status=status.HTTP_200_OK,
+                data=UserSerializer(instance=user).data,
+            )
+        return Response(
+            status=status.HTTP_400_BAD_REQUEST,
+            data={"ok": False, "errors": "Неверно указаны значения"},
+        )
 
     @action(methods=["POST"], detail=False, url_name="phone_login", permission_classes=[])
     def phone_login(self, request):
-        """Авторизация по телефону после валидации кода."""
-        serializer = self.get_serializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        if not User.objects.filter(phone=serializer.validated_data["phone"]):
-            return Response(status=status.HTTP_404_NOT_FOUND, message=USER_NOT_FOUND)
+        """Авторизация по телефону"""
+        serializer: UserLoginOrRegisterSerializer = self.get_serializer(data=request.data)
+        if not serializer.is_valid():
+            return Response({"ok": False, "errors": serializer.errors})
         user = authenticate(request, **serializer.data)
         custom_login(request, user, backend="users.backends.phone.PhoneBackend")
         return Response(status=status.HTTP_200_OK, data=UserSerializer(instance=user).data)
 
     @action(detail=False, methods=["GET"])
     def logout(self, request):
-        """Логаут."""
         logout(request)
         return Response(status=status.HTTP_200_OK)
+
+    @action(detail=False, methods=["PATCH"])
+    def change(self, request):
+        if not bool(request.user and request.user.is_authenticated):
+            return Response({"ok": False, "errors": "not authorized"})
+        user = request.user
+        serializer = self.get_serializer(instance=user, data=request.data, partial=True)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
