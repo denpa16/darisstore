90b77fd2fca6c16de8be63d3cd1b911a555ae475
Denis Pavlov <54680228+denpa16@users.noreply.github.com>
2025-08-20 12:58:37 +0300

    Update routers.py
    Add router

--- backend/config/routers.py
+++ backend/config/routers.py
@@ -1,8 +1,9 @@
 from rest_framework.routers import SimpleRouter
 
-from users.viewsets import UserViewSet
+from users.viewsets import UserViewSet, OTPTokenViewSet
 
 router = SimpleRouter()
 
 # A -Z sorting
+router.register(r"otp", OTPTokenViewSet, "otp")
 router.register(r"users", UserViewSet, "users")
